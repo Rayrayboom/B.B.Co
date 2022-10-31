86513d6d68ca45fbc6719680bf9f1af20cb43dbe
rayrayboom <rayrayboom@hotmail.com.tw>
2022-10-31 20:11:26 +0800

    feat: add datePicker to addNewData page

--- BreadAndBacon_CoAccounting/AddNewDataPage/AddNewDataTableViewCell.swift
+++ BreadAndBacon_CoAccounting/AddNewDataPage/AddNewDataTableViewCell.swift
@@ -7,29 +7,50 @@
 
 import UIKit
 
+protocol PassTextfieldDelegate: AnyObject {
+    func passTextField(_ cell: AddNewDataTableViewCell, sender: UIDatePicker)
+}
+
 class AddNewDataTableViewCell: UITableViewCell {
-    @IBOutlet weak var dateLabel: UILabel!
+    weak var delegate: PassTextfieldDelegate?
+
+    @IBOutlet weak var dateTextfield: UITextField!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var contentTextField: UITextField!
-    @IBOutlet weak var categoryLabel: UILabel!
-    @IBOutlet weak var categoryResultLabel: UILabel!
-    @IBOutlet weak var accountLabel: UILabel!
+    @IBOutlet weak var qrButton: UIButton!
+    @IBAction func insertQRCode(_ sender: UIButton) {
+    }
     @IBOutlet weak var detailTextView: UITextView!
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
-        // Configure the view for the selected state
     }
 
     // 金額、種類、帳戶function - 生成tableview時覆用
     func fillInContent(name: String) {
         titleLabel.text = name
         contentTextField.text = ""
+    }
+
+    // cell本身不執行func，只在這邊設定完之後交由delegate傳過去給VC動作
+    // 把datePicker相關動作移回來是因為addTarget的self指的是當前的VC裡的func，在addNewDataVC執行addTarget會報錯的原因在於self在addNewDataVC中其實會找不到dateTextfield這個東西，會無法給值; 因此需要用一個config func讓addTarget抓到cell本身的func且拿到對應dateTextfield，才有辦法把sender的值用delegate倒給addNewDataVC去塞值
+    func config(dateStr: String) {
+        self.dateTextfield.text = dateStr
+        self.dateTextfield.textColor = .link
+        let datePicker = UIDatePicker()
+        datePicker.datePickerMode = .date
+        datePicker.addTarget(self, action: #selector(didSelectData(_:)), for: UIControl.Event.valueChanged)
+        datePicker.frame.size = CGSize(width: 0, height: 200)
+        self.dateTextfield.inputView = datePicker
+    }
+
+    // 利用addNewDataTableViewCell自己的delegate傳值過去給addNewDataVC來執行塞值的動作
+    @objc func didSelectData(_ sender: UIDatePicker) {
+        self.delegate?.passTextField(self, sender: sender)
     }
 }
--- BreadAndBacon_CoAccounting/AddNewDataPage/AddNewDataViewController.swift
+++ BreadAndBacon_CoAccounting/AddNewDataPage/AddNewDataViewController.swift
@@ -9,7 +9,9 @@ import UIKit
 import SwiftUI
 
 class AddNewDataViewController: UIViewController {
-    let category: [String] = ["時間", "金額", "種類", "帳戶", ""]
+    //因為DateFormatter()非常佔記憶體也很吃效能，因此把他從cellForRowAt拉出來，放在global，這樣只要宣告一次就好，否則每次gen tableView就得生成一次
+    let formatter = DateFormatter()
+    let category: [String] = ["金額", "種類", "帳戶"]
 
     @IBOutlet weak var addNewDadaTableView: UITableView!
 
@@ -23,6 +25,9 @@ class AddNewDataViewController: UIViewController {
 
         // segmentControl 偵測改值狀態
         sourceSegmentControl.addTarget(self, action: #selector(handelSegmentControl), for: .valueChanged)
+        // 點選X時，執行取消新增
+        cancelNewData()
+        formatter.dateFormat = "dd/MM/yyyy"
     }
 
     // func for segmentControl 更改時切換頁面
@@ -31,13 +36,24 @@ class AddNewDataViewController: UIViewController {
 
 //        switch sourceSegmentControl.selectedSegmentIndex {
 //        case 0:
-//            
+//
 //        case 1:
-//            
+//
 //        default:
 //            <#code#>
 //        }
     addNewDadaTableView.reloadData()
+    }
+
+    // 取消新增資料按鈕trigger
+    func cancelNewData() {
+        navigationItem.leftBarButtonItem = UIBarButtonItem(
+            image: UIImage(systemName: "xmark"), style: .plain, target: self, action: #selector(dismissPage))
+    }
+
+    // 取消新增func並dismiss VC
+    @objc func dismissPage() {
+        self.presentingViewController?.dismiss(animated: true, completion: nil)
     }
 }
 
@@ -45,11 +61,6 @@ extension AddNewDataViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print(indexPath)
     }
-
-
-//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-//        return
-//    }
 }
 
 extension AddNewDataViewController: UITableViewDataSource {
@@ -76,73 +87,48 @@ extension AddNewDataViewController: UITableViewDataSource {
             else {
                 fatalError("can not create cell")
             }
-            dateCell.dateLabel.text = ""
+
+            dateCell.delegate = self
+            let date = Date()
+            // formatter把日期(date)轉成String塞給dateStr
+            let dateStr = formatter.string(from: date)
+            // 把存著date的dateStr用cell的func config()塞值給cell裡面的textField
+            dateCell.config(dateStr: dateStr)
             return dateCell
         } else if indexPath.section == 1 {
-            if indexPath.row == 0 {
-                guard let addDataCell = tableView.dequeueReusableCell(
-                    withIdentifier: "addDataCell") as? AddNewDataTableViewCell
-                else {
-                    fatalError("can not create cell")
-                }
-
-                addDataCell.fillInContent(name: category[indexPath.row])
-                return addDataCell
-            } else if indexPath.row == 1 {
-                guard let addDataCell = tableView.dequeueReusableCell(
-                    withIdentifier: "addDataCell") as? AddNewDataTableViewCell
-                else {
-                    fatalError("can not create cell")
-                }
-
-                addDataCell.fillInContent(name: category[indexPath.row])
-                return addDataCell
-            } else if indexPath.row == 2 {
-                guard let addDataCell = tableView.dequeueReusableCell(
-                    withIdentifier: "addDataCell") as? AddNewDataTableViewCell
-                else {
-                    fatalError("can not create cell")
-                }
-
-                addDataCell.fillInContent(name: category[indexPath.row])
-                return addDataCell
-            } else {
-                guard let addDataCell = tableView.dequeueReusableCell(
-                    withIdentifier: "addDataCell") as? AddNewDataTableViewCell
-                else {
-                    fatalError("can not create cell")
-                }
-
-                addDataCell.fillInContent(name: category[indexPath.row])
-                return addDataCell
+            guard let addDataCell = tableView.dequeueReusableCell(
+                withIdentifier: "addDataCell") as? AddNewDataTableViewCell
+            else {
+                fatalError("can not create cell")
             }
+            // 依照category array裡的資料筆數決定section:1有幾個cell
+            addDataCell.fillInContent(name: category[indexPath.row])
+            return addDataCell
         } else if indexPath.section == 2 {
             guard let qrCell = tableView.dequeueReusableCell(
                 withIdentifier: "QRCell") as? AddNewDataTableViewCell
             else {
                 fatalError("can not create cell")
             }
-            qrCell.dateLabel.text = ""
+//            qrCell.qrButton.setImage(UIImage(systemName: "qrcode.viewfinde"), for: .normal)
             return qrCell
         } else {
-            if indexPath.row == 0 {
-                guard let detailCell = tableView.dequeueReusableCell(
-                    withIdentifier: "detailCell") as? AddNewDataTableViewCell
-                else {
-                    fatalError("can not create cell")
-                }
-                detailCell.detailTextView.text = "711"
-                return detailCell
-            } else {
-                guard let detailCell = tableView.dequeueReusableCell(
-                    withIdentifier: "detailCell") as? AddNewDataTableViewCell
-                else {
-                    fatalError("can not create cell")
-                }
-                detailCell.detailTextView.text = "711"
-//                tableView.rowHeight = 300
-                return detailCell
+            guard let detailCell = tableView.dequeueReusableCell(
+                withIdentifier: "detailCell") as? AddNewDataTableViewCell
+            else {
+                fatalError("can not create cell")
             }
+            detailCell.detailTextView.text = "Lorem ipsum dolor sit er elit lamet, consectetaur"
+            tableView.rowHeight = 250
+            return detailCell
         }
+    }
+}
+
+extension AddNewDataViewController: PassTextfieldDelegate {
+    // 用delegate把cell和點選的sender傳過來，進行給新值的動作
+    @objc func passTextField(_ cell: AddNewDataTableViewCell, sender: UIDatePicker) {
+        // 當date picker改變時，執行此func，把當前改變的date塞給textfield
+        cell.dateTextfield.text = formatter.string(from: sender.date)
     }
 }
